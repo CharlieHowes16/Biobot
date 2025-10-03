ece19d829e7d04fe1cc034bd1444c9f4f184e31a
CharlieHowes16 <charliejhowes@gmail.com>
2025-10-03 20:56:14 +0100

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,6 @@
+# Chatbot variables
+chatbot_greeting_displayed = False
+chatbot_message = ""
+chatbot_message_box = pygame.Rect(270, 110, 860, 60)
+chatbot_user_typing_box = pygame.Rect(260, 700, 600, 50)
+chatbot_user_typing_text = ""
