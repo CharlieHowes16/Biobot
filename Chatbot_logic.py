65b142961ed384ee7cbb357fae28c29352c6a0ce
CharlieHowes16 <charliejhowes@gmail.com>
2025-11-26 21:09:43 +0000

    Refactor chatbot logic and remove unused variables

--- Chatbot_logic.py
+++ Chatbot_logic.py
@@ -1,12 +1,7 @@
-import pygame
 import difflib
+from database.setup_database import setup_database
 
-# Chatbot variables
-chatbot_greeting_displayed = False
-chatbot_message = ""
-chatbot_message_box = pygame.Rect(270, 110, 860, 60)
-chatbot_user_typing_box = pygame.Rect(260, 700, 600, 50)
-chatbot_user_typing_text = ""
+biology_notes = setup_database()
 
 # Retreives the definition for the term the user asks for 
 def biology_term_definition(collect_biology_term):
@@ -18,26 +13,22 @@ def biology_term_definition(collect_biology_term):
         if term.lower() == clear_terms:
             return definition
     
-    # If no exact match, find closest match using difflib
-    closest_matches = difflib.get_close_matches(clear_terms, terms, n=1, cutoff=0.6)
+    # If no exact match, find closest match using fuzzy search 
+    closest_matches = difflib.get_close_matches(clear_terms, terms, n=1, cutoff=0.6) # Words need to be at least 60% correct 
     if closest_matches:
         # Get index of closest match
         closest_term = closest_matches[0]
         for term, definition in biology_notes:
             if term.lower() == closest_term:
-                return f"{definition}"
+                definition = f"{definition}"
+                return definition
             
     
-    return "Sorry, I couldn't find that term."
-
-def xp_popup_animation(amount):
-    global xp_popup_text, xp_popup_start_time
-    xp_popup_text = f"+{amount} XP"
-    xp_popup_start_time = pygame.time.get_ticks()
+    return "Sorry, I couldn't find that term." 
 
 # Removes any common words like the so sorting questions is more managable
 def blacklist_fillers(user_input):
-    filler_words = {"what", "is", "the", "a", "an", "define", "meaning", "of", "please"}
+    filler_words = {"what", "is", "the", "a", "an", "define", "meaning", "of", "please"} # Words to blacklist 
     words_entered = user_input.lower().split()
     filtered_words = [word for word in words_entered if word not in filler_words]
-    return "".join(filtered_words)
+    return " ".join(filtered_words) # Rejoins the sentance without blacklisted words
