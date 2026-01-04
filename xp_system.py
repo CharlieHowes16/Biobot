a8ae27e63c3ae20ba2641d17e1d8352a8e3626dc
CharlieHowes16 <charliejhowes@gmail.com>
2026-01-04 18:13:49 +0000

    Add audio for level up and modify xp_popup_animation

--- xp_system.py
+++ xp_system.py
@@ -1,8 +1,11 @@
 import sqlite3
+import pygame
 
-# XP popup animation variables
-xp_popup_text = ""
-xp_popup_start_time = 0
+pygame.mixer.init() 
+celebration_sound = pygame.mixer.Sound("celebration.mp3") 
+
+def level_up_audio(): 
+    celebration_sound.play()
 
 def xp_system(username, points_gained):
     xp_database_connection = sqlite3.connect('user_details_database.db')
@@ -15,6 +18,13 @@ def xp_system(username, points_gained):
 
     # Update with new XP
     new_xp = current_xp + points_gained
+
+    # Level up detection
+    old_level = level_up_system(current_xp) 
+    new_level = level_up_system(new_xp) 
+    if new_level > old_level: 
+        level_up_audio()
+
     xp_cursor.execute("UPDATE user_details SET XP = ? WHERE Username = ?", (new_xp, username))
     xp_database_connection.commit()
     xp_database_connection.close()
@@ -30,7 +40,8 @@ def level_up_system(xp):
             break
     return level - 1  # Adjust for initial level
 
-def xp_popup_animation(amount):
-    global xp_popup_text, xp_popup_start_time
+def xp_popup_animation(amount, xp_popup_text, xp_popup_start_time):
     xp_popup_text = f"+{amount} XP"
     xp_popup_start_time = pygame.time.get_ticks()
+    
+    return xp_popup_text, xp_popup_start_time
